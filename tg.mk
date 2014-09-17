6d1ee6e62cf3c019517fdafd26811aa9c3adca39
Andres <apascual89@gmail.com>
2014-09-16 22:17:37 -0400

    Gummify

---
+++ tg.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/samsung/hltespr/full_hltespr.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hltespr
+PRODUCT_NAME := tg_hltespr
